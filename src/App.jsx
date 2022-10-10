93df094f4db1dfe62986564ad1f6e4bf2c21e2a1
Bill Bryan Baduya <115074618+bcube90@users.noreply.github.com>
2022-10-10 17:05:50 +0800

    Intialized app with bare minimu.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Fragment} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      asdf asdf  asdf
+    </Fragment>
+  );
+}
+
+export default App;
